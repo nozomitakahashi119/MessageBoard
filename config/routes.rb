6542a3f1360f2e9932dcf79a34bd615cc46e92af
nozomi.takahashi <hgoopde624@yahoo.co.jp>
2020-02-23 07:01:43 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+=begin
+  # CRUD
+  get 'messages/:id', to: 'messages#show'
+  post 'messages', to: 'messages#cretae'
+  put 'messages/:id', to: 'messages#update'
+  delete 'messages/:id', to: 'messages#destroy'
+  
+  # index: showの補助ページ
+  get 'messages', to: 'messages#index'
+  
+  # new: 新規作成用のフォーム
+  get 'messages/new', to: 'messages#new'
+  
+  # edit: 更新用のフォーム
+  get 'messages/:id/edit', to: 'messages#edit'
+=end
+
+  #トップページの指定
+  root to: 'messages#index'
+
+  #上記は次の記述で補完できる
+  resources :messages
+  
 end
